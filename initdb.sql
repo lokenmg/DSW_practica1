e79d63d9859a789b0248cbe8a41fb7a640832f55
YACRUZ <yamatsukino@gmail.com>
2023-08-29 19:24:57 -0600

    Add PHP-apache

--- initdb.sql
+++ initdb.sql
@@ -1,11 +1,15 @@
 CREATE USER myuser;
 CREATE DATABASE mydb;
 GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
+ALTER DATABASE mydb OWNER TO myuser;
 
-CREATE TABLE ejemplo
+\connect mydb
+CREATE TABLE mytable
 (
 	clave integer NOT NULL,
 	nombre character varying,
 	direccion character varying,
 	CONSTRAINT pk_clave PRIMARY KEY (clave) 
 );
+
+ALTER TABLE mytable OWNER TO myuser; 
